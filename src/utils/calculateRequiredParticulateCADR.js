dcdcca2572ed265d80b62cfd887928d869e75849
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2026-02-12 03:09:30 +0500

    Change calculateRequiredParticulateCADR to use object parameters

--- src/utils/calculateRequiredParticulateCADR.js
+++ src/utils/calculateRequiredParticulateCADR.js
@@ -1,10 +1,10 @@
-const calculateRequiredParticulateCADR = (
+const calculateRequiredParticulateCADR = ({
   indoorParticulateConcentrationLimit,
   outdoorParticulateConcentration,
   ventilationRate,
   indoorParticulateGenerationRate,
   penetrationFactor = 1,
-) => {
+}) => {
   if (indoorParticulateConcentrationLimit <= 0) {
     throw new Error("Indoor particulate concentration limit must be greater than zero.");
   }
@@ -23,11 +23,11 @@ const outdoorPm2_5Concentration = 100; // µg/m³
 const ventilationRate = 70; // m³/h
 const indoorParticulateGenerationRate = 1000; // µg per hour
 
-const requiredCADR = calculateRequiredParticulateCADR(
-  indoorPm2_5ConcentrationLimit,
-  outdoorPm2_5Concentration,
+const requiredCADR = calculateRequiredParticulateCADR({
+  indoorParticulateConcentrationLimit: indoorPm2_5ConcentrationLimit,
+  outdoorParticulateConcentration: outdoorPm2_5Concentration,
   ventilationRate,
   indoorParticulateGenerationRate
-);
+});
 
 console.log(`Required CADR for PM2.5: ${requiredCADR.toFixed(2)} m³/h`);
