45b0dce598b6b9eea349a84eed195924ad3ca84c
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2026-02-19 01:11:36 +0500

    Add countries dataset with currency information

---
+++ src/data/countries.js
@@ -0,0 +1,9 @@
+const countries = [
+  { code: 'CN', name: 'China', currency: 'CNY' },
+  { code: 'KZ', name: 'Kazakhstan', currency: 'KZT' },
+  { code: 'RU', name: 'Russia', currency: 'RUB' },
+  { code: 'UK', name: 'United Kingdom', currency: 'GBP' },
+  { code: 'US', name: 'United States', currency: 'USD' },
+];
+
+export default countries;
