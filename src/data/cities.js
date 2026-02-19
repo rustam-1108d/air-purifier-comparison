6378218290cd0a2d36decfbca8ffffeb3756331c
rustam-1108d <133682982+rustam-1108d@users.noreply.github.com>
2026-02-19 22:10:14 +0500

    Add src/data/cities.js

---
+++ src/data/cities.js
@@ -0,0 +1,9 @@
+const cities = [
+  { id: 'SHA', countryCode: 'CN', name: 'Shanghai' },
+  { id: 'ALA', countryCode: 'KZ', name: 'Almaty' },
+  { id: 'MOW', countryCode: 'RU', name: 'Moscow' },
+  { id: 'LON', countryCode: 'UK', name: 'London' },
+  { id: 'NYC', countryCode: 'US', name: 'New York City' },
+];
+
+export default cities;
